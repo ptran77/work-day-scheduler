1d6d25895da15f2339006e9e7bf56b97f96503b6
Peter Tran <pettran866@gmail.com>
2022-04-19 13:55:30 -0700

    Save event

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let currentDay = $("#currentDay");
 currentDay.text(moment().format("dddd, MMMM Do"));
 let timeBlocks = $(".time-block");
+let saveEvents = {};
 
 
 // function to format time blocks with past, present, or future
@@ -20,5 +21,14 @@ let formatTimeBlocks = function() {
     }
   }
 }
+
+timeBlocks.on('click','.saveBtn', function() {
+  if($(this).parent().find(".textarea").text() != "") {
+    if(!saveEvents[currentDay.text()]) {
+      saveEvents[currentDay.text()] = {};
+    }
+  saveEvents[currentDay.text()][$(this).parent().find(".hour").text()] = $(this).parent().find(".textarea").text();
+  }
+})
 
 formatTimeBlocks();
