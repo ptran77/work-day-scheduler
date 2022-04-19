3ec3acdc53e21d3ee1170acbe118cc3ddef243cc
Peter Tran <pettran866@gmail.com>
2022-04-19 13:03:22 -0700

    color coded timeblocks with past, present, future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,23 @@
 let currentDay = $("#currentDay");
 currentDay.text(moment().format("dddd, MMMM Do"));
+
+let formatTimeBlocks = function() {
+  let currentHour = moment().format("hA");
+  let hours = $(".hour");
+  let textAreas = $(".textarea");
+  let curHourFound = false;
+  for(let i = 0; i < hours.length; i++) {
+    if(hours.eq(i).text() === currentHour) {
+      textAreas.eq(i).addClass("present");
+      curHourFound = true;
+    }
+    else if (curHourFound) {
+      textAreas.eq(i).addClass("future");
+    }
+    else {
+      textAreas.eq(i).addClass("past");
+    }
+  }
+}
+
+formatTimeBlocks();
